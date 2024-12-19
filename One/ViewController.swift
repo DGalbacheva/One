7a23b4d321f82b131099be86befa0b7977debc93
Doroteya Galbacheva <galbachevva@gmail.com>
2024-12-19 19:46:33 +0200

    one

---
+++ One/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  One
+//
+//  Created by MacBook Pro 15 on 19.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
